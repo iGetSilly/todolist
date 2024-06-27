eaa0a287c2af302dbed3b8d203bc2b1ec089ea92
Leonardo Dutra <iuoyyp@gmail.com>
2024-06-27 08:14:10 -0300

    Menu hamburger, estilo finalizado, ainda falta colocar os links

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function alteraIcon() {
         hamIcon.style.backgroundImage = "url('img/Ham icon.svg')";
     }
 }
+
+// Fazendo o menu aparecer ao clicar no botao hamburger
+function changeVisibility() {
+    let off = document.querySelector(".off")
+    let currentVisibility = window.getComputedStyle(off).visibility
+    if (currentVisibility.includes("hidden")) {
+        off.style.visibility = "visible"
+        off.style.scale = 1
+    } else {
+        off.style.visibility = "hidden"
+        off.style.scale = 0
+    }
+}
