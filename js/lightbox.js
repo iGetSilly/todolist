754249d3902fac2142cfba4a1e1661a34aeb32b8
Leonardo Dutra <iuoyyp@gmail.com>
2024-07-10 19:48:54 -0300

    lightbox quase finalizado, falta somente adicionar a logica para a troca de imagens

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,50 +1,62 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    function getSrc() {
-        document.querySelectorAll("main .img").forEach(img => {
-            img.addEventListener('click', function () {
-                let src = this.src;
-                let main = document.querySelector(".container");
-                let lightbox = document.querySelector(".lightbox");
-                let mainImg = document.querySelector(".mainImg");
-                if (main && lightbox && mainImg) {
-                    if (main.classList.contains("displayOn")) {
-                        main.classList.remove("displayOn");
-                        main.classList.add("displayOff");
-                        lightbox.classList.remove("displayOff");
-                        lightbox.classList.add("displayOn");
-                        mainImg.src = src;
-                    }
-                }
-            });
-        });
-    }
     getSrc();
+    getSrcLightbox()
+    changeImg()
 });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    function getSrcLightbox() {
-        document.querySelectorAll("section div .img").forEach(img => {
-            img.addEventListener('click', function () {
-                let src = this.src
-                let mainImg = document.querySelector(".mainImg")
-                if (mainImg) {
-                    if (mainImg.src !== src) {
-                        mainImg.src = src
-                    }
+function getSrc() {
+    document.querySelectorAll("main .img").forEach(img => {
+        img.addEventListener('click', function () {
+            let src = this.src;
+            let main = document.querySelector(".container");
+            let lightbox = document.querySelector(".lightbox");
+            let mainImg = document.querySelector(".mainImg");
+            if (main && lightbox && mainImg) {
+                if (main.classList.contains("displayOn")) {
+                    main.classList.remove("displayOn");
+                    main.classList.add("displayOff");
+                    lightbox.classList.remove("displayOff");
+                    lightbox.classList.add("displayOn");
+                    mainImg.src = src;
                 }
-            })
+            }
+        });
+    });
+}
+
+
+function getSrcLightbox() {
+    document.querySelectorAll("section div .img").forEach(img => {
+        img.addEventListener('click', function () {
+            let src = this.src
+            let mainImg = document.querySelector(".mainImg")
+            if (mainImg) {
+                if (mainImg.src !== src) {
+                    mainImg.src = src
+                }
+            }
         })
-    }
-    getSrcLightbox()
-})
+    })
+}
 
 function expandMainImg() {
     let mainImg = document.querySelector(".mainImg")
     let lightbox = document.querySelector(".lightbox")
+    let icon_left = document.querySelector(".icon_left")
+    let icon_right = document.querySelector(".icon_right")
     if (lightbox.classList.contains("displayOn")) {
-        document.querySelectorAll("section div .img").forEach(img => {
+        document.querySelectorAll(".lightbox_images .img").forEach(img => {
             img.classList.add("displayOff")
         })
     }
     mainImg.classList.add("expandMainImg")
+    icon_left.classList.remove("displayOff")
+    icon_left.classList.add("displayOn")
+    icon_right.classList.remove("displayOff")
+    icon_right.classList.add("displayOn")
+
+}
+
+function changeImg() {
+    
 }
